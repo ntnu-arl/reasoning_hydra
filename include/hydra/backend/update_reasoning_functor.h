6bc7a5c9ccea7488f30ae592c781685c903bb3d7
Albert Gassol <albertgassol1@gmail.com>
2024-11-12 15:46:08 +0100

    Refactor reasoning functor

--- include/hydra/backend/update_reasoning_functor.h
+++ include/hydra/backend/update_reasoning_functor.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <filesystem>
 
+#include <glog/logging.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+
 #include <kimera_pgmo/kimera_pgmo_interface.h>
 #include "hydra/backend/update_functions.h"
 #include "hydra/reasoning/reasoning_config.h"
 #include "hydra/common/shared_module_state.h"
 #include "hydra/utils/nearest_neighbor_utilities.h"
+#include "hydra/utils/timing_utilities.h"
 
 namespace hydra {
 
@@ -16,7 +21,9 @@ struct UpdateReasoningFunctor {
                  const UpdateInfo::ConstPtr& info);
 
   private:
-    bool detectRoomChange(NodeId& room_to_reason, const SharedDsgInfo& dsg);
+  bool detectRoomChange(NodeId& room_to_reason, const SharedDsgInfo& dsg);
+  void getObjectPointcloud(const SharedDsgInfo& dsg, const SceneGraphNode& room, pcl::PointCloud<pcl::PointXYZRGBL>::Ptr object_cloud) const;
+  void savePointCloud(const pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& cloud, const std::string& cloud_name, const std::string& format) const;
 
     ThreeDSSGConfig config_;
     SharedModuleState::Ptr state_;
--- src/backend/update_reasoning_functor.cpp
+++ src/backend/update_reasoning_functor.cpp
@@ -34,11 +34,6 @@
  * -------------------------------------------------------------------------- */
 #include "hydra/backend/update_reasoning_functor.h"
 
-#include <glog/logging.h>
-#include <pcl/io/pcd_io.h>
-
-#include "hydra/utils/timing_utilities.h"
-
 namespace hydra {
 
 
@@ -81,8 +76,64 @@ MergeList UpdateReasoningFunctor::call(const DynamicSceneGraph&,
     const auto& room_to_reason = dsg.graph->getLayer(DsgLayers::ROOMS).getNode(room_to_reason_id);
     // Iterate over the objects in the room, store a pointcloud of the objects
     pcl::PointCloud<pcl::PointXYZRGBL>::Ptr object_cloud(new pcl::PointCloud<pcl::PointXYZRGBL>);
+    getObjectPointcloud(dsg, room_to_reason, object_cloud);
+    // Save pointcloud to file
+    std::filesystem::path object_cloud_path = std::filesystem::path(config_.input_folder) / (room_to_reason.attributes<SemanticNodeAttributes>().name + ".pcd");
+    pcl::io::savePCDFileBinary(object_cloud_path.string(), *object_cloud);
+    return {};
+}
 
-    for (const auto& place_id : room_to_reason.children()) {
+bool UpdateReasoningFunctor::detectRoomChange(NodeId& room_to_reason_id, const SharedDsgInfo& dsg) {
+
+    // Get place nodes that have parents (i.e. are in rooms)
+    std::vector<NodeId> latest_places_vec;
+    std::vector<Eigen::Vector3f> place_centroids;
+    const auto& places = dsg.graph->getLayer(DsgLayers::PLACES).nodes();
+    
+    for (const auto& [place_id, place] : places) {
+        if (NodeSymbol(place_id).category() == 'p' && place->hasParent()) {
+            place_centroids.emplace_back(place->attributes<NodeAttributes>().position.cast<float>());
+            latest_places_vec.emplace_back(place_id);
+        }
+    }
+
+    if (place_centroids.empty()) {
+        return false;
+    }
+    
+    if (!neighbor_search_) {
+        neighbor_search_ = std::make_unique<PointNeighborSearch>(place_centroids);
+    } else {
+        neighbor_search_.reset(new PointNeighborSearch(place_centroids));
+    }
+    
+    // Find the closest place to the current pose
+    size_t closest_place_id;
+    float distance_squared;
+    const auto current_pose = dsg.graph->dynamicLayersOfType(DsgLayers::AGENTS).begin()->second->getPositionByIndex(dsg.graph->dynamicLayersOfType(DsgLayers::AGENTS).begin()->second->numNodes() - 1).cast<float>();
+    bool nn_success = neighbor_search_->search(current_pose, distance_squared, closest_place_id);
+    
+    if (!nn_success) {
+        return false;
+    }
+
+    const auto& closest_place_node_id = latest_places_vec[closest_place_id];
+    NodeId closest_room_node_id = *(places.at(closest_place_node_id)->parents().begin());
+
+    // If the closest place is in the same room as we were previously, then we haven't changed rooms
+    if (closest_room_node_id == prev_room_node_id_) {
+        return false;
+    }
+    
+    room_to_reason_id = prev_room_node_id_;
+    prev_room_node_id_ = closest_room_node_id;
+    
+    return true;
+}
+
+void UpdateReasoningFunctor::getObjectPointcloud(const SharedDsgInfo& dsg, const SceneGraphNode& room, pcl::PointCloud<pcl::PointXYZRGBL>::Ptr object_cloud) const {
+    // Iterate over the objects in the room, store a pointcloud of the objects
+    for (const auto& place_id : room.children()) {
         if (!dsg.graph->getLayer(DsgLayers::PLACES).hasNode(place_id)) {
             continue;
         }
@@ -116,57 +167,14 @@ MergeList UpdateReasoningFunctor::call(const DynamicSceneGraph&,
             }
         }
     }
-
-    // Save pointcloud to file
-    std::filesystem::path object_cloud_path = std::filesystem::path(config_.input_folder) / (room_to_reason.attributes<SemanticNodeAttributes>().name + ".pcd");
-    pcl::io::savePCDFileBinary(object_cloud_path.string(), *object_cloud);
-    return {};
 }
 
-bool UpdateReasoningFunctor::detectRoomChange(NodeId& room_to_reason_id, const SharedDsgInfo& dsg) {
-
-    // If the previous room is not in the latest places, then we will have detected a room change
-    std::vector<NodeId> latest_places_vec;
-    std::vector<Eigen::Vector3f> place_centroids;
-    const auto& places = dsg.graph->getLayer(DsgLayers::PLACES).nodes();
-    
-    for (const auto& [place_id, place] : places) {
-        if (NodeSymbol(place_id).category() == 'p' && place->hasParent()) {
-            place_centroids.emplace_back(place->attributes<NodeAttributes>().position.cast<float>());
-            latest_places_vec.emplace_back(place_id);
-        }
+void UpdateReasoningFunctor::savePointCloud(const pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& cloud, const std::string& cloud_name, const std::string& format = ".pcd") const {
+    std::string object_cloud_path = config_.input_folder + cloud_name + format;
+    if (format == ".pcd") {
+        pcl::io::savePCDFileBinary(object_cloud_path, *cloud);
+    } else if (format == ".ply") {
+        pcl::io::savePLYFile(object_cloud_path, *cloud, true);
     }
-
-    if (place_centroids.empty()) {
-        return false;
-    }
-    
-    if (!neighbor_search_) {
-        neighbor_search_ = std::make_unique<PointNeighborSearch>(place_centroids);
-    } else {
-        neighbor_search_.reset(new PointNeighborSearch(place_centroids));
-    }
-    
-    size_t closest_place_id;
-    float distance_squared;
-    const auto current_pose = dsg.graph->dynamicLayersOfType(DsgLayers::AGENTS).begin()->second->getPositionByIndex(dsg.graph->dynamicLayersOfType(DsgLayers::AGENTS).begin()->second->numNodes() - 1).cast<float>();
-    bool nn_success = neighbor_search_->search(current_pose, distance_squared, closest_place_id);
-    
-    if (!nn_success) {
-        return false;
-    }
-    
-    const auto& closest_place_node_id = latest_places_vec[closest_place_id];
-    NodeId closest_room_node_id = *(places.at(closest_place_node_id)->parents().begin());
-
-    if (closest_room_node_id == prev_room_node_id_) {
-        return false;
-    }
-    
-    room_to_reason_id = prev_room_node_id_;
-    prev_room_node_id_ = closest_room_node_id;
-    
-    return true;
 }
-
 }  // namespace hydra
