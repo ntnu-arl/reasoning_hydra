4f6fdb5c3fe1b22380d9a01e8b77668cdeecbb74
albert <albertgassol1@gmail.com>
2025-07-15 16:01:47 +0200

    Changes to use recorded data easily

--- include/hydra/input/input_conversion.h
+++ include/hydra/input/input_conversion.h
@@ -47,5 +47,32 @@ bool convertColor(InputData& data);
  */
 void convertVertexMap(InputData& data, bool in_world_frame);
 
+
+class RvlCodec {
+ public:
+  RvlCodec();
+  // Compress input data into output. The size of output can be equal to
+  // (1.5 * numPixels + 4) in the worst case.
+  int CompressRVL(const unsigned short* input, unsigned char* output,
+                  int numPixels);
+  // Decompress input data into output. The size of output must be
+  // equal to numPixels.
+  void DecompressRVL(const unsigned char* input, unsigned short* output,
+                     int numPixels);
+
+ private:
+  RvlCodec(const RvlCodec&);
+  RvlCodec& operator=(const RvlCodec&);
+
+  void EncodeVLE(int value);
+  int DecodeVLE();
+
+  int *buffer_;
+  int *pBuffer_;
+  int word_;
+  int nibblesWritten_;
+};
+
+
 }  // namespace conversions
 }  // namespace hydra
